9bfc9f2f45ed838bfc71db9b9cc362bc2d8fb46a
egangbo <merveillesgangbo@gmail.com>
2025-07-01 10:57:27 +0200

    ML exercice 1

--- ML/app.py
+++ ML/app.py
@@ -1,20 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+
+# Example streamlit interface
+import streamlit as st
 import joblib
 import pandas as pd
 
-class TitanicInput(BaseModel):
-    Pclass: int
-    Sex: str
-    Age: float
-    Fare: float
-    Embarked: str
-
-app = FastAPI()
 model = joblib.load("titanic_pipeline.pkl")
+Pclass = st.selectbox("Pclass", [1, 2, 3])
+Sex = st.selectbox("Sex", ["male", "female"])
+Age = st.slider("Age", 0, 100, 25)
+Fare = st.slider("Fare", 0.0, 500.0, 32.0)
+Embarked = st.selectbox("Embarked", ["S", "C", "Q"])
 
-@app.post("/predict")
-def predict(data: TitanicInput):
-    df = pd.DataFrame([data.dict()])
-    prediction = model.predict(df)[0]
-    return {"prediction": int(prediction)}
+if st.button("Predict"):
+    X_new = pd.DataFrame([[Pclass, Sex, Age, Fare, Embarked]],
+            columns=["Pclass", "Sex", "Age", "Fare", "Embarked"])
+    pred = model.predict(X_new)
+    st.write("Prediction:", "Survived" if pred[0] == 1 else "Did not survive")
